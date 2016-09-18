04a2607ab8c5ac111269752e3646005718c67f81
lefort11 <gplaxin@mail.ru>
2016-09-19 00:03:52 +0300

    SOME_SHIT_WAS_MADE

--- src/Source/Program.cpp
+++ src/Source/Program.cpp
@@ -4,6 +4,7 @@
 #include "yx2Framework.h"
 #include "Presentation.h"
 #include <string>
+#include <iostream>
 #pragma comment(lib, "d3d9.lib")
 
 namespace dxm = glm;
@@ -17,32 +18,47 @@ namespace dxm = glm;
  * \param nCmdShow Unused WinAPI parameter.
  * \return Exit code.
  */
+
+
+
+
+
 int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine,
-					 _In_ int nCmdShow)
+	_In_ int nCmdShow)
 {
 
 
 
 	(void)hInstance, hPrevInstance, lpCmdLine, nCmdShow;
 
-	yx2::framework::Window mainWindow({100, 100, 1280, 720});
-	auto presentation = mainWindow.Direct3D9<Presentation>({0, 0, 1280, 720});
+	
 
-	yx2::framework::Window secndWindow({ 100, 100, 1280, 720 }, nullptr, true);
-	wchar_t* buf = new wchar_t[6];
-	mbstowcs_s(nullptr, buf, 6, "Начало демонстрации", _TRUNCATE);
-	secndWindow.Button({300, 350,700,40 }, buf, nullptr);
-	secndWindow.Button({ 300, 400, 700, 40 }, buf, nullptr);
-	secndWindow.Button({ 300, 450, 700, 40 }, buf, nullptr);
+	yx2::framework::Window mainWindow({ 100, 100, 1280, 720 });
+	mainWindow.TextEdit({ 300, 100, 700, 20 }, L"Московский Государственный Университет", yx2::framework::TextEditFlags::CenterAlignment);
+	mainWindow.Image({ 20, 20, 100,100 }, L"../cmc.jpg");
+	mainWindow.Button({300, 350,700,40 }, L"Начало", [mainWindow](long)
+	{
+		auto presentation = mainWindow.Direct3D9<Presentation>({ 0, 0, 1280, 720 });
+		MSG msg;
+		while (GetMessage(&msg, nullptr, 0, 0))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
 
-
+			presentation->Update();
+		}
+	});
+	mainWindow.Button({ 300, 400, 700, 40 }, L"Люблю Олега", nullptr);
+	mainWindow.Button({ 300, 450, 700, 40 }, L"Выход", [](long)
+	{
+		exit(0);
+	});
+	
 	MSG msg;
 	while (GetMessage(&msg, nullptr, 0, 0))
 	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
-
-		presentation->Update();
 	}
 
 	return 0;
