f566bd14abaf83e794a966bffb9e926705345d5a
lefort11 <gplaxin@mail.ru>
2016-09-20 22:40:39 +0300

    Some buttons appeared

--- src/Source/Program.cpp
+++ src/Source/Program.cpp
@@ -33,12 +33,20 @@ int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_ HINSTANCE hPrevInstance, _In
 
 	
 
-	yx2::framework::Window mainWindow({ 100, 100, 1280, 720 });
-	mainWindow.TextEdit({ 300, 100, 700, 20 }, L"Московский Государственный Университет", yx2::framework::TextEditFlags::CenterAlignment);
-	mainWindow.Image({ 20, 20, 100,100 }, L"../cmc.jpg");
-	mainWindow.Button({300, 350,700,40 }, L"Начало", [mainWindow](long)
+	yx2::framework::Window mainWindow({ 100, 100, 1280, 720 }, nullptr, true);
+
+	std::array<yx2::framework::WindowWidget, 10> windowWidgets;
+	windowWidgets[0] = mainWindow.TextEdit({ 500, 100, 700, 20 }, L"Московский Государственный Университет им. М.В. Ломоносова",
+													yx2::framework::TextEditFlags::CenterAlignment);
+	windowWidgets[1] = mainWindow.Image({ 10, 15, 100,100 }, L"../cmc1.bmp");
+	windowWidgets[2] = mainWindow.Image({ 1525, 15, 100, 100 }, L"../fizfak.bmp");
+	windowWidgets[3] = mainWindow.Button({500, 500,700,40 }, L"Начало", [&mainWindow, &windowWidgets](long)
 	{
+		for(auto i = 0; i < 10; ++i )
+			windowWidgets[i].Destroy();
+
 		auto presentation = mainWindow.Direct3D9<Presentation>({ 0, 0, 1280, 720 });
+
 		MSG msg;
 		while (GetMessage(&msg, nullptr, 0, 0))
 		{
@@ -47,13 +55,13 @@ int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_ HINSTANCE hPrevInstance, _In
 
 			presentation->Update();
 		}
-	});
-	mainWindow.Button({ 300, 400, 700, 40 }, L"Люблю Олега", nullptr);
-	mainWindow.Button({ 300, 450, 700, 40 }, L"Выход", [](long)
-	{
-		exit(0);
-	});
 	
+	});
+	windowWidgets[4] = mainWindow.Button({ 500, 550, 700, 40 }, L"Люблю Олега", nullptr);
+	windowWidgets[5] = mainWindow.Button({ 500, 600, 700, 40 }, L"Выход", [](long)
+	{
+		PostQuitMessage(0);
+	});	
 	MSG msg;
 	while (GetMessage(&msg, nullptr, 0, 0))
 	{
