7e3c1782d14f277969a3f83fe0faeae42c3c0ad6
James Jhuighuy <butakovoleg@gmail.com>
2016-11-14 09:21:27 +0300

    RC6.5.1

--- gfx/Shaders/BlendTexturesFXAA.hlsl
+++ gfx/Shaders/BlendTexturesFXAA.hlsl
@@ -7,38 +7,35 @@ texture TextureTransparent;
 sampler SamplerTransparent = sampler_state
 {
 	Texture = <TextureTransparent>;
-	/*AddressU = Clamp;
-	AddressV = Clamp;
 	MinFilter = Linear;
-	MagFilter = Linear;*/
+	MagFilter = Linear;
 };	// sampler SamplerTransparent
 
 texture TextureSolid;
 sampler SamplerSolid = sampler_state
 {
 	Texture = <TextureSolid>;
-	/*AddressU = Clamp;
-	AddressV = Clamp;
 	MinFilter = Linear;
-	MagFilter = Linear;*/
+	MagFilter = Linear;
 };	// sampler SamplerSolid
 
 /* Blends two textures from our render targets. */
-float3 BlendRenderTargets(float2 texCoords)
+float4 BlendRenderTargets(float2 texCoords)
 {
 	const float4 transparent = tex2D(SamplerTransparent, texCoords);
 	const float4 solid = tex2D(SamplerSolid, texCoords);
 
 	const float4 sourceBlendColor = solid;
 	const float4 destBlendColor = solid * transparent;
-	return lerp(sourceBlendColor, destBlendColor, transparent.a).xyz;
+	return lerp(sourceBlendColor, destBlendColor, transparent.a);
 }
 
 /* Performs AA for our render solid target. */
 float4 AntiAliasBlendedRenderTargets(float2 texCoords)
 {
 	static const float2 texCoordOffset = 1.0f / float2(1440.0f * 0.75f, 990.0f);
-	static const float3 luminance = float3(0.299f, 0.587f, 0.114f);
+//	static const float2 texCoordOffset = 1.0f / float2(1920.0f * 0.75f, 1080.0f);
+	static const float4 luminance = float4(0.299f, 0.587f, 0.114f, 0.0f);
 
 	/* Computing luminance in the surrounding texels.. */
 	const float luminanceCenter = dot(luminance, BlendRenderTargets(texCoords + texCoordOffset));
@@ -50,7 +47,7 @@ float4 AntiAliasBlendedRenderTargets(float2 texCoords)
 	const float minLuminance = min(luminanceCenter, min(luminanceTopLeft, min(luminanceTopRight, min(luminanceBottomLeft, luminanceBottomRight))));
 	const float maxLuminance = max(luminanceCenter, max(luminanceTopLeft, max(luminanceTopRight, max(luminanceBottomLeft, luminanceBottomRight))));
 
-	static const float dirReduceMul = 1.0f / 1.0f;
+	static const float dirReduceMul = 1.0f / 4.0f;
 	static const float dirReduceMin = 1.0f / 128.0f;
 	static const float2 dirSpanMax = float2(8.0f, 8.0f);
 
@@ -58,24 +55,24 @@ float4 AntiAliasBlendedRenderTargets(float2 texCoords)
 	float2 blurDirection = float2((luminanceTopLeft - luminanceTopRight) + (luminanceBottomLeft - luminanceBottomRight)
 		, (luminanceTopLeft - luminanceBottomLeft) - (luminanceTopRight - luminanceBottomRight));
 
-	const float blurDirectionReduce = max((luminanceTopLeft + luminanceTopRight + luminanceBottomLeft + luminanceBottomRight) * 0.25f * dirReduceMul, dirReduceMin);
+	const float blurDirectionReduce = max((luminanceTopLeft + luminanceTopRight + luminanceBottomLeft + luminanceBottomRight) * dirReduceMul, dirReduceMin);
 	const float blurInverseDirectionAdjustment = 1.0f / (blurDirectionReduce + min(abs(blurDirection.x), abs(blurDirection.y)));
 	blurDirection = clamp(blurInverseDirectionAdjustment * blurDirection, -dirSpanMax, dirSpanMax) * texCoordOffset;
 	
 	/* Applying blur.. */
-	const float3 oneStepBlur = 0.5f * (
+	const float4 oneStepBlur = 0.5f * (
 		BlendRenderTargets(texCoords + blurDirection * (1.0f / 3.0f - 0.5f)) +
 		BlendRenderTargets(texCoords + blurDirection * (2.0f / 3.0f - 0.5f)));
-	const float3 twoStepBlur = 0.5f * oneStepBlur + 0.25f * (
+	const float4 twoStepBlur = 0.5f * oneStepBlur + 0.25f * (
 		BlendRenderTargets(texCoords + blurDirection * (0.0f / 3.0f - 0.5f)) +
 		BlendRenderTargets(texCoords + blurDirection * (3.0f / 3.0f - 0.5f)));
 	
-	const float luminanceTwoStep = dot(luminance, twoStepBlur);
+	/*const float luminanceTwoStep = dot(luminance, twoStepBlur);
 	if (minLuminance < luminanceTwoStep && luminanceTwoStep < maxLuminance)
 	{
-		return float4(twoStepBlur, 1.0f);
-	}
-	return float4(oneStepBlur, 1.0f);
+		return twoStepBlur;
+	}*/
+	return twoStepBlur;
 }
 
 float4 main(float2 texCoords : TEXCOORD0) : COLOR
